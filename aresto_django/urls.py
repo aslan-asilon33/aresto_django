2ab8bf66c6f20cdfc703cc69db8967db73fbe001
Sulaslan Setiawan <sulaslan.setiawan@balitower.co.id>
2023-09-16 23:52:51 +0700

    first commit

---
+++ aresto_django/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from user.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user', include('user.urls')),
+]
